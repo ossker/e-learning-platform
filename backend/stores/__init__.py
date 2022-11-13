fdac7522d0b8c4fbe96b0b57da1f85e4293d087c
ossker <oskar.bienko@onet.pl>
2022-11-13 15:15:15 +0100

    Factories, Stores interfaces removed.

--- backend/stores/__init__.py
+++
@@ -1,4 +0,0 @@
-from stores.ICategoryStore import ICategoryStore
-from stores.ICourseStore import ICourseStore
-from stores.ITutorialStore import ITutorialStore
-from stores.IUserStore import IUserStore
