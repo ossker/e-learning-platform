1f2ace7ae508056a9614127a45f3d78ed99ecd23
ossker <oskar.bienko@onet.pl>
2022-11-26 15:30:30 +0100

    Courses resource fetched.

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {Link} from 'react-router-dom'
 import { useAuth } from '../auth'
+import Course from './Course'
 
 const LoggedInHome=()=>{
+    const [courses, setCourses] = useState([]);
+
+    useEffect(
+        ()=>{
+            fetch('/course/courses')
+            .then(res=>res.json())
+            .then(data=>{
+                console.log(data)
+                setCourses(data)
+            })
+            .catch(err=>console.log(err))
+        },[]
+    );
+
     return(
         <div className='courses'>
             <h1>List of courses.</h1>
+            {
+                courses.map(
+                    (course)=>(
+                        <Course name={course.name} description={course.description} owner={course.owner}/>
+                    )
+                )
+            }
         </div>
     )
 }
